22955b12c8d9234d58487ae60b01af01462c3cd5
NewCpr <johncpr1295@gmail.com>
2020-11-18 08:33:16 -0800

    everything we planed to do

--- frontend/script.js
+++ frontend/script.js
@@ -6,12 +6,23 @@ const cont = document.getElementById("container")
 
 function RenderData(data) {
     data.forEach((r) => {
-        const recard = document.createElement("div")
+        const card = document.createElement("div")
         const title = document.createElement("h2")
         title.innerText = r.title
         const image = document.createElement("img")
-        image.setAttribute("src", "banner-img")
+        image.classList.add("image")
+        image.setAttribute("src", r.image)
         const ingredients = document.createElement("p")
         ingredients.innerText = r.ingredients
+        card.appendChild(title)
+        card.appendChild(image)
+        card.appendChild(ingredients)
+
+        card.classList.add("card")
+        
+        title.classList.add("retitle")
+        ingredients.classList.add("ingred")
+
+        cont.appendChild(card)
     });
 }
